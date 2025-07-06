bb488fd879a5b64d9c50a42f3c80b190ef8843c8
David Vogt <dv036@hdm-stuttgart.de>
2025-07-06 20:18:08 +0200

    Initial commit

---
+++ src/main/java/de/davidvogt/hkbmod/HkbMod.java
@@ -0,0 +1,46 @@
+package de.davidvogt.hkbmod;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraftforge.eventbus.api.listener.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(HkbMod.MOD_ID)
+public final class HkbMod {
+    public static final String MOD_ID = "hkbmod";
+
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+
+    public HkbMod(FMLJavaModLoadingContext context) {
+        var modBusGroup = context.getModBusGroup();
+        FMLCommonSetupEvent.getBus(modBusGroup).addListener(this::commonSetup);
+
+        BuildCreativeModeTabContentsEvent.getBus(modBusGroup).addListener(HkbMod::addCreative);
+        context.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+
+    }
+
+    // Add the example block item to the building blocks tab
+    private static void addCreative(BuildCreativeModeTabContentsEvent event) {
+
+    }
+
+    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+    @Mod.EventBusSubscriber(modid = MOD_ID, value = Dist.CLIENT)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+
+        }
+    }
+}
